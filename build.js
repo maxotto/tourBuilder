cf28c97fd8ecee0708afdef7b1318c30b55c5e28
maxotto <agmfromita@mail.ru>
2018-10-03 16:39:41 +0300

    Buttons of florr selector moved up by 70 px

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const Builder = require('./tourBuilder');
 const FtpConfig = require('./ftpConfig');
 const config = {
-  inFolder: './inFolder',
+  inFolder: './inFolder_224hollywoodave',
   outFolder: './outFolder',
   templatesFolder: './templates',
   googleApiKey: 'AIzaSyARIMiX_C7rE4U-pM6nih2n2z2z0YfhrfY',
@@ -11,9 +11,15 @@ const config = {
   loadingtext: '',
   // googleMapUnits: 'METRIC',
   googleMapUnits: 'IMPERIAL',
+  //43.76841812480301, lng->-79.39991750129661
+  // 43,7604; -79,4116
   mapCenter: {
     lat: 43.7604,
     lng: -79.4116
+  },
+  floorMapShift:{
+    x: 0,
+    y: 60,
   },
   floorSelect: [
     {
